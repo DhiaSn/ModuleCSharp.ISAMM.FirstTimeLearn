fb4caf53f7055b6be2534b0db71f0dd5d1b68644
Dhou <dhiaeddsn@outlook.com>
2025-11-01 12:28:34 +0100

    Add respository + get service

---
+++ ModuleC#.ISAMM.COCOJV/Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using ModuleC_.ISAMM.COCOJV.DTOs.UserDTOs;
+using ModuleC_.ISAMM.COCOJV.Entities;
+using ModuleC_.ISAMM.COCOJV.Repositories;
+
+namespace ModuleC_.ISAMM.COCOJV.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public List<GetAllUserResponse> GetAll()
+        {
+            return _userRepository
+                .GetAll()
+                .Select(u => new GetAllUserResponse(u))
+                .ToList();
+        }
+
+        public GetUserByIdResponse GetById(Guid id)
+        {
+            User user = _userRepository.GetById(id);
+            if (user == null)
+            {
+                return null;
+            }
+            return new GetUserByIdResponse(user);
+        }
+
+        // --> TO DO: 
+        /*
+         * POST 
+         * PUT
+         * Delete
+         */
+        // ----------------
+    }
+}
